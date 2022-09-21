bcd501717f4a0b728402c56566d732f9a94b1ea4
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-20 21:27:54 -0300

    modificado API para filtrar a questao pelo ID passado pela URL

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,5 +1,19 @@
 import questoes from "../bancoDeQuestoes"
 
 export default function handler(req, res) {
-    res.status(200).json(questoes[0].converterParaObjeto())
+  //pega o id passado pela url
+  const idSelecionado = +req.query.id
+
+  //pega o array para ir a condição se é igual ao passado pelo id
+  const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+  //se tiver 1 tem questão, sem tamanho então sem questão
+  if(unicaQuestaoOuNada.length === 1){
+    const questaoSelecionada = unicaQuestaoOuNada[0]
+    res.status(200).json(questaoSelecionada.converterParaObjeto())
+  } else {
+    //não existe o ID, por isso um 204 de sem conteudo
+    res.status(204).send()
   }
+    
+}
