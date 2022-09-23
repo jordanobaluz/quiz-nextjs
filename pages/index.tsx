a30f686c97acd63124a82efdde5f1da47f92ef87
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-22 21:58:50 -0300

    criado componente botao que ira receber uma referencia para a proxima questao

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import Botao from '../components/Botao'
 import Questao from '../components/Questao'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
@@ -33,6 +34,7 @@ export default function Home() {
     <div style={
       {
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh'
@@ -43,6 +45,7 @@ export default function Home() {
         respostaFornecida={respostaFornecida}
         tempoEsgotado={tempoEsgotado}
       />
+      <Botao texto='Próxima' href='/resultado'></Botao>
     </div>
   )
 }
